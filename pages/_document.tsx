27e638bda1793897ff97b2b6a43534e0d972fb2b
MuniS97 <tursunovmunis141@gmail.com>
2025-04-16 16:55:33 +0500

    -

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<title>Rustam & Zilola</title>
+			<title>Firdavs & Marjona</title>
 			<Head />
 			<body>
 				<Main />
